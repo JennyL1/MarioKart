6387224c26548b9cc8ed269ba4313ae00cfe38a9
Jenny Li <wangjunqian@wangjunqiandeMacBook-Pro-2.local>
2019-03-08 17:25:16 -0500

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by Jenny Li on 3/8/19.
+//  Copyright © 2019 Jenny Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
